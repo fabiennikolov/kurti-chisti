215fcad47798d45e07f020a57529acc266a9e5d4
fabiennikolov <fabiennikolov9@gmail.com>
2023-11-02 23:20:58 +0200

    button

--- components/Nav.vue
+++ components/Nav.vue
@@ -14,6 +14,7 @@ function toggleMobileMenu() {
 <template>
   <div class="sticky top-0 z-100 bg-transparent backdrop-blur">
     <div class="mx-auto w-full flex items-center justify-between border-white/9 px-4 py-4 sm:px-12">
+      <DarkToggle />
       <div class="flex justify-center gap-8 lg:text-1rem">
         <NuxtLink to="/">
           <img src="@/logo.png" class="my-3 inline-block max-w-35 lg:max-w-45">
@@ -21,10 +22,10 @@ function toggleMobileMenu() {
       </div>
       <div class="flex items-center justify-between space-x-4">
         <button class="text-white lg:hidden" @click="toggleMobileMenu">
-          <UnoIcon icon="i-iconamoon-menu-burger-horizontal" class="h8 w8" />
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="1.5" d="M20 7H4m16 5H4m16 5H4" /></svg>
         </button>
         <div class="hidden text-sm text-white/80 lg:flex space-x-4">
-          <a class="block items-center justify-center rounded-md bg-blue-600 px-4 py-2 text-4 font-bold" href="tel:+359879883894">Обадете се сега: 0879883894</a>
+          <a class="block items-center justify-center b-1 b-white rounded-md bg-blue-600 px-4 py-2 text-4 font-bold" href="tel:+359879883894">Обадете се сега: 0879883894</a>
           <template v-for="item in menuItems" :key="item.id">
             <NuxtLink :to="item.route" class="px2 py2 xl:hover:(rounded-md bg-white/9)" @click="toggleMobileMenu">
               {{ item.text }}
@@ -35,7 +36,7 @@ function toggleMobileMenu() {
     </div>
 
     <div v-if="showMobileMenu" class="grid b-white/9 p-4 lg:hidden space-y-2">
-      <a class="inline-block w-63 items-center justify-center rounded-md bg-blue-600 px-4 py-2" href="tel:+359879883894">Обадете се сега: 0879883894</a>
+      <a class="inline-block w-64 items-center justify-center b-1 b-white rounded-md bg-blue-600 px-4 py-2" href="tel:+359879883894">Обадете се сега: 0879883894</a>
       <template v-for="item in menuItems" :key="item.id">
         <NuxtLink :to="item.route" class="text-white/80 hover:(rounded-3 bg-white/9)" @click="toggleMobileMenu">
           {{ item.text }}
