0930e52ea6d66270c61440632383d1cd5d93bde1
fabiennikolov <fabiennikolov9@gmail.com>
2023-11-04 00:04:58 +0200

    adding content for index

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,15 @@
 <template>
-  asd
+  <img alt="Снимка на Кърти чисти и извозва" width="1905" height="700" src="/background.jpg" class="absolute inset-0 h-200 w-full object-cover">
+  <div class="relative mx-auto max-w-5xl flex px-4 py-44 text-4xl font-bold">
+    <div class="relative z-10 bg-black/60 py-4 sm:py-24">
+      <h1 class="mx-auto mb-12 flex px-4 text-center text-4xl font-bold font-bold text-white/90 sm:text-5xl">
+        Кърти чисти и извозва в град София и околността
+      </h1>
+      <a class="mx-auto w-fit flex text-2xl font-bold sm:text-4xl" href="tel:+359879883894">
+        <CoolButton>
+          0879883894
+        </CoolButton>
+      </a>
+    </div>
+  </div>
 </template>
