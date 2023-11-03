128e22e02c24285182a26803701456420fcb409a
fabiennikolov <fabiennikolov9@gmail.com>
2023-11-04 00:03:30 +0200

    adding button component

---
+++ components/CoolButton.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+withDefaults(defineProps<{
+  intent?: 'primary' | 'accent' | 'unstyled'
+  overlayClass?: string
+}>(), {
+  intent: 'primary',
+})
+</script>
+
+<template>
+  <button
+    class="group relative isolate overflow-hidden px-6 py-2 btn"
+    :class="{ 'text-white border-1 border-white': intent !== 'unstyled' }"
+  >
+    <div
+      class="absolute -bottom-4 -left-10 -right-10 -top-4 -z-1"
+      :class="[{ 'bg-blue text-[#222222]': !overlayClass }, overlayClass]"
+      rounded="50%"
+      translate-y="100%" group-hover="translate-y-0"
+      flex items-center justify-center transition-all-500
+    >
+      <slot />
+    </div>
+    <span class="invisible">
+      <slot />
+    </span>
+    <span
+
+      group-hover="-translate-y-50% opacity-0"
+      absolute inset-0 flex items-center justify-center transition-all-500
+    >
+
+      <slot />
+    </span>
+  </button>
+</template>
