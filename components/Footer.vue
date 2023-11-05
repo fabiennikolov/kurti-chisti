534850bc5e21a98ba59ab27fab1de3186c302388
fabiennikolov <fabiennikolov9@gmail.com>
2023-11-05 20:32:14 +0200

    adding content for footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,16 +1,16 @@
 <template>
   <div
-    class="mx-a flex items-center justify-center gap-14 b-t-1 b-black px-4 py-14 .dark:b-t-1 .dark:b-white/25 .dark:bg-white/8 .light:bg-gray-600/25"
+    class="mx-a items-center justify-center gap-14 b-t-1 b-black px-4 px-a py-14 md:flex .dark:b-t-1 .dark:b-white/25 .dark:bg-white/8 .light:bg-gray-600/25"
   >
     <NuxtLink to="/">
-      <img src="@/logo.webp" class="inline-block h-10 w-12 sm:h-16 sm:w-16">
+      <img src="@/logo.webp" class="m-4 inline-block h-10 h-24 w-12 w-24 flex gap-4">
     </NuxtLink>
     <a href="tel:+359879883894">
-      <CoolButton class="text-3 font-bold xl:text-4">
+      <CoolButton class="mx-a mb-4 items-center justify-center text-3 font-bold xl:text-4">
         Обадете се сега: 0879883894
       </CoolButton>
     </a>
-    <ul class="flex flex-col text-xl">
+    <ul class="flex-col md:flex md:text-3 xl:text-xl">
       <li>
         <NuxtLink to="/">
           Начало
