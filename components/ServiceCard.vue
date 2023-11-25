8d99c3a732cc541510d3ea39bcc954fdf5d04a66
fabiennikolov <fabiennikolov9@gmail.com>
2023-11-25 15:11:21 +0200

    s

---
+++ components/ServiceCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="mx-auto max-w-75 w-full overflow-hidden b-1px b-2 b-black rounded-3 p-4 drop-shadow-xl space-y-6 hover:bg-gray/7 sm:p-8 hover:shadow-xl"
+  >
+    <h4 class="text-(center 2xl) font-(extrabold) leading-relaxed">
+      Service name
+    </h4>
+    <div flex="~ col" class="items-center gap-3">
+      <div class="h-40 w-50">
+        <img src="/sectionimg2.png" class="mx-auto h-full object-contain">
+      </div>
+      <button>
+        <a
+          target="_blank"
+        >
+          Link to other content
+        </a>
+      </button>
+    </div>
+    <p class="mt-10px text-center leading-7">
+      basic description
+    </p>
+  </div>
+</template>
---
+++ pages/Services.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="grid grid-cols-3 mx-auto mt-4 items-center justify-center gap-4">
+    <ServiceCard />
+    <ServiceCard />
+    <ServiceCard />
+    <ServiceCard />
+    <ServiceCard />
+    <ServiceCard />
+  </div>
+</template>
