7796e9fb96d8293b35efeb93fa328ae4f6fb11a6
fabiennikolov <fabiennikolov9@gmail.com>
2024-04-05 02:29:40 +0300

    changing favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -57,9 +57,9 @@ export default defineNuxtConfig({
     head: {
       viewport: 'width=device-width,initial-scale=1',
       link: [
-        { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
-        { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' },
-        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+        { rel: 'icon', href: '/favicon.svg', sizes: 'any' },
+        { rel: 'icon', type: 'image/png', href: '/assets/images/favicon.png' },
+        { rel: 'apple-touch-icon', href: '/assets/images/favicon.png' },
       ],
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
