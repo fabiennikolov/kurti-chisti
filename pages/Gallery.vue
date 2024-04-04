79b52bf5f7860ca732df5a78c1337785739e86d7
fabiennikolov <fabiennikolov9@gmail.com>
2024-04-04 23:39:12 +0300

    fixing gallery grid

--- pages/Gallery.vue
+++ pages/Gallery.vue
@@ -3,7 +3,7 @@
     <h1 class="mb-8 text-center text-4xl font-bold">
       Галерия на нашите обекти
     </h1>
-    <div class="grid grid-cols-4 gap-4">
+    <div class="grid grid-cols-1 gap-4 px-4 lg:grid-cols-2 xl:grid-cols-4">
       <img
         width="379" height="279" class="aspect-[4/3] h-full w-full rounded-3 object-cover"
         src="~/assets/images/Gallery/1.webp" alt="Снимка на кърти чисти извозва"
