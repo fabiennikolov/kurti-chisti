5d831838b02a7bfedd9f0716964d2cdf02d2d497
fabiennikolov <fabiennikolov9@gmail.com>
2024-04-04 23:01:17 +0300

    removing link from gallery

--- pages/Gallery.vue
+++ pages/Gallery.vue
@@ -4,90 +4,45 @@
       Галерия на нашите обекти
     </h1>
     <div class="grid grid-cols-4 gap-4">
-      <a
-        href="~/assets/images/Gallery/1.webp"
-        data-fancybox="gallery"
-        class="group relative overflow-hidden rounded-lg shadow-md"
+      <img
+        width="379" height="279" class="aspect-[4/3] h-full w-full rounded-3 object-cover"
+        src="~/assets/images/Gallery/1.webp" alt="Снимка на кърти чисти извозва"
       >
-        <div class="absolute inset-0 bg-black opacity-0 transition-opacity duration-300 duration-500 group-hover:opacity-25" />
-        <img
-          width="379"
-          height="279"
-          class="aspect-[4/3] h-full w-full object-cover"
-          src="~/assets/images/Gallery/1.webp"
-          alt="Снимка на кърти чисти извозва"
-        >
-      </a>
-      <a
-        href="~/assets/images/Gallery/2.webp"
-        data-fancybox="gallery"
-        class="group relative overflow-hidden rounded-lg shadow-md"
+      <div
+        class="absolute inset-0 bg-black opacity-0 transition-opacity duration-300 duration-500 group-hover:opacity-25"
+      />
+      <img
+        width="379" height="279" class="aspect-[4/3] h-full w-full rounded-3 object-cover"
+        src="~/assets/images/Gallery/2.webp" alt="Снимка на кърти чисти извозва"
       >
-        <div class="absolute inset-0 bg-black opacity-0 transition-opacity duration-300 duration-500 group-hover:opacity-25" />
-        <img
-          width="379"
-          height="279"
-          class="aspect-[4/3] h-full w-full object-cover"
-          src="~/assets/images/Gallery/2.webp"
-          alt="Снимка на кърти чисти извозва"
-        >
-      </a>
-      <a
-        href="~/assets/images/Gallery/3.webp"
-        data-fancybox="gallery"
-        class="group relative overflow-hidden rounded-lg shadow-md"
+      <div
+        class="absolute inset-0 bg-black opacity-0 transition-opacity duration-300 duration-500 group-hover:opacity-25"
+      />
+      <img
+        width="379" height="279" class="aspect-[4/3] h-full w-full rounded-3 object-cover"
+        src="~/assets/images/Gallery/3.webp" alt="Снимка на кърти чисти извозва"
       >
-        <div class="absolute inset-0 bg-black opacity-0 transition-opacity duration-300 duration-500 group-hover:opacity-25" />
-        <img
-          width="379"
-          height="279"
-          class="aspect-[4/3] h-full w-full object-cover"
-          src="~/assets/images/Gallery/3.webp"
-          alt="Снимка на кърти чисти извозва"
-        >
-      </a>
-      <a
-        href="~/assets/images/Gallery/4.webp"
-        data-fancybox="gallery"
-        class="group relative overflow-hidden rounded-lg shadow-md"
+      <div
+        class="absolute inset-0 bg-black opacity-0 transition-opacity duration-300 duration-500 group-hover:opacity-25"
+      />
+      <img
+        width="379" height="279" class="aspect-[4/3] h-full w-full rounded-3 object-cover"
+        src="~/assets/images/Gallery/4.webp" alt="Снимка на кърти чисти извозва"
       >
-        <div class="absolute inset-0 bg-black opacity-0 transition-opacity duration-300 duration-500 group-hover:opacity-25" />
-        <img
-          width="379"
-          height="279"
-          class="aspect-[4/3] h-full w-full object-cover"
-          src="~/assets/images/Gallery/4.webp"
-          alt="Снимка на кърти чисти извозва"
-        >
-      </a>
-      <a
-        href="~/assets/images/Gallery/5.webp"
-        data-fancybox="gallery"
-        class="group relative overflow-hidden rounded-lg shadow-md"
+      <div
+        class="absolute inset-0 bg-black opacity-0 transition-opacity duration-300 duration-500 group-hover:opacity-25"
+      />
+      <img
+        width="379" height="279" class="aspect-[4/3] h-full w-full rounded-3 object-cover"
+        src="~/assets/images/Gallery/5.webp" alt="Снимка на кърти чисти извозва"
       >
-        <div class="absolute inset-0 bg-black opacity-0 transition-opacity duration-300 duration-500 group-hover:opacity-25" />
-        <img
-          width="379"
-          height="279"
-          class="aspect-[4/3] h-full w-full object-cover"
-          src="~/assets/images/Gallery/5.webp"
-          alt="Снимка на кърти чисти извозва"
-        >
-      </a>
-      <a
-        href="~/assets/images/Gallery/6.webp"
-        data-fancybox="gallery"
-        class="group relative overflow-hidden rounded-lg shadow-md"
+      <div
+        class="absolute inset-0 bg-black opacity-0 transition-opacity duration-300 duration-500 group-hover:opacity-25"
+      />
+      <img
+        width="379" height="279" class="aspect-[4/3] h-full w-full rounded-3 object-cover"
+        src="~/assets/images/Gallery/6.webp" alt="Снимка на кърти чисти извозва"
       >
-        <div class="absolute inset-0 bg-black opacity-0 transition-opacity duration-300 duration-500 group-hover:opacity-25" />
-        <img
-          width="379"
-          height="279"
-          class="aspect-[4/3] h-full w-full object-cover"
-          src="~/assets/images/Gallery/6.webp"
-          alt="Снимка на кърти чисти извозва"
-        >
-      </a>
     </div>
   </div>
 </template>
