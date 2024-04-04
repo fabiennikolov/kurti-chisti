c7551b9f21f65fe2c297671b2920c8289f680951
fabiennikolov <fabiennikolov9@gmail.com>
2024-04-04 22:26:51 +0300

    adding card propts type for services card list

--- types.ts
+++ types.ts
@@ -4,3 +4,12 @@ export interface SocialLink {
   to: string
   icon?: string
 }
+
+export interface CardProps {
+  title: string
+  description?: string
+  image: string
+  link: {
+    redirect: string
+  }
+}
