ee92172fa857e4eebfdb63ae09291053abe8e0b8
fabiennikolov <fabiennikolov9@gmail.com>
2024-04-04 23:08:30 +0300

    adding margin y to service card section

--- components/ServiceCardSection.vue
+++ components/ServiceCardSection.vue
@@ -7,7 +7,7 @@ defineProps<{
 </script>
 
 <template>
-  <div class="grid grid-cols-1 mx-auto mt-12 justify-items-center gap-4 px-4 container lg:grid-cols-4 md:grid-cols-2 lt-lg:w-fit sm:px-0">
+  <div class="grid grid-cols-1 mx-auto my-12 justify-items-center gap-4 px-4 container lg:grid-cols-4 md:grid-cols-2 lt-lg:w-fit sm:px-0">
     <ServiceCard
       v-for="card in cards" :key="card.title"
       v-bind="card"
